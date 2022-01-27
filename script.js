23732ec82fb7b93e1a1d57610e02bc380ca7d5c2
Aisuluu Asanovaclea <aisuluu.asanova.ss@gmail.com>
2022-01-27 21:05:48 +0600

    2les

---
+++ script.js
@@ -0,0 +1,22 @@
+//1 задача
+let text = "ddd@bbb@ccc";
+let result = text.replaceAll("@", "!");
+document.write(result + "<br>");
+
+//2 задача
+let str = "js";
+let str2 = str.toUpperCase();
+document.write(str2 + "<br>");
+
+//3 задача
+
+let str3 = str2.toLowerCase();
+document.write(str3 + "<br>");
+
+//4 задача
+let txt = "I am a hero!"
+document.write(txt.length)
+
+//5 задача
+/*let userCard = prompt('Номер вашей карты?')
+document.write("Номер вашей карты: " + userCard.replace(userCard.slice(0, -4), "************"));*/
