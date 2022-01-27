4ec7475bf6ba0c3da7fd4d5eb2d21f2a0ac7adbf
aisuluu-a <89789635+aisuluu-a@users.noreply.github.com>
2022-01-27 21:20:42 +0600

    Add files via upload

---
+++ 1week/1less/script.js
@@ -0,0 +1,51 @@
+
+
+//1 задача
+let number1 = 10;
+document.write(number1 + "<br>");
+
+
+//2 задача
+let userName = "Aisuluu ";
+document.write(userName + "<br>")
+userName = 500;
+document.write(userName + "<br>")
+
+//3 задача
+/*let myName = "Aisuluu";
+    myMail = "aisuluu@gmail.com";
+document.write(` My name is ${myName} and my e-mail is ${myMail} <br>`);*/
+
+//4 задача
+/*let cc = prompt("Введите количество сувениров");
+let dd = prompt("Введите количество безделушек");
+let c = Number(cc);
+let d = Number(dd);
+const f = 75;
+const g = 112;
+
+document.write(`Количество сувениров ${c} <br>`);
+document.write(`Количество безделушек ${d} <br>`);
+document.write(`Вес одного сувенира ${f} граммов <br>`);
+document.write(`Вес одной безделушки ${g} грамм <br>`);
+
+let sum = f*c+g*d;
+
+document.write(`Общий вес сувениров и безделушек: ${sum.toFixed(2)}г. <br>`);
+consol.log(sum);*/
+
+//5 задача
+let aa = prompt ("Задайте значение для a");
+let bb = prompt ("Задайте значение для b");
+let a = Number(aa);
+let b = Number(bb);
+document.write("a=" + a + "<br>");
+document.write("b=" + b + "<br>");
+document.write("a+b =" + (a + b) + "<br>");
+document.write("a-b =" + (a - b) + "<br>");
+document.write("a*b =" + (a * b) + "<br>");
+document.write("a/b =" + (a / b) + "<br>");
+document.write("частное от деления a/b =" + Math.trunc(a / b) + "<br>");
+document.write("остаток от деления a*b="+ (a % b) + "<br>");
+document.write("a^b =" + (a ** b) + "<br>");
+consol.log(a / b);
